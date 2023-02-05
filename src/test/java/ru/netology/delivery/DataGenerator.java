47d231b6870c7d1862e083c00d36206ecc146806
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2023-02-05 19:27:18 +0400

    fourth commit

--- src/test/java/ru/netology/delivery/DataGenerator.java
+++ src/test/java/ru/netology/delivery/DataGenerator.java
@@ -48,7 +48,7 @@ public class DataGenerator {
         }
 
         public static UserInfo generateUser(String locale) {
-            return new UserInfo(generateCity(). generateName(locale), generatePhone(locale));
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
         }
     }
 
